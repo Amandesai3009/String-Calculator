a4813707cad77b0ff9a1dfdabc2aec84e10f133c
Amandesai3009 <aman.desai3005@gmail.com>
2025-07-04 22:17:41 +0530

    Test 2: Handle single number input

--- src/stringcalculator.js
+++ src/stringcalculator.js
@@ -1,7 +1,12 @@
 function add(numbers){
+
+    //first test case
     if(numbers === '') {
         return 0;
     }
+
+    //second test case
+    if(!numbers.includes(",")) return parseInt(numbers, 10);
 }
 
 
--- test/stringcalculator.test.js
+++ test/stringcalculator.test.js
@@ -3,3 +3,7 @@ const { add } = require('../src/stringcalculator');
 test('empty string returns 0', () => {
   expect(add('')).toBe(0);
 });
+
+test('returns number itself for single number string', () => {
+  expect(add("1")).toBe(1);
+});
